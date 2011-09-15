3695db1caf79602ca4b7119a19fa449355b54806
Chris <chris@starforge.co.uk>
2011-09-15 17:02:13 +0100

    Started work on core logic module.

---
+++ blocks/MegaphoneCore.pm
@@ -0,0 +1,37 @@
+## @file
+# This file contains the implementation of the core megaphone features.
+#
+# @author  Chris Page &lt;chris@starforge.co.uk&gt;
+# @version 1.0
+# @date    15 Sept 2011
+# @copy    2011, Chris Page &lt;chris@starforge.co.uk&gt;
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package MegaphoneCore;
+
+use base qw(Block); # This class extends Block
+
+
+# ============================================================================
+#  Interface functions
+
+## @method $ page_display()
+# Generate the page content for this module.
+sub page_display {
+    my $self = shift;
+
+    
+}
+
+1;
