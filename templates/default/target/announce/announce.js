6c64279329f6b2d9f166bed4569c7ea451ef5615
Chris <chris@starforge.co.uk>
2011-11-15 16:13:43 +0000

    Added basic message fragment for announcements to test date stuff.

---
+++ templates/default/target/announce/announce.js
@@ -0,0 +1,24 @@
+function date_disable(datefield, control) {
+    var checked = $(control).get("checked");
+
+    $(datefield).set("disabled", checked);
+}
+
+window.addEvent('domready', function() {
+    Locale.use('en-GB')
+    new Picker.Date($('open_date'), { 
+                        timePicker: true, 
+                        positionOffset: {x: 5, y: 0}, 
+                        pickerClass: 'datepicker_dashboard', 
+                        useFadeInOut: !Browser.ie 
+    });
+    new Picker.Date($('close_date'), { 
+                        timePicker: true, 
+                        positionOffset: {x: 5, y: 0}, 
+                        pickerClass: 'datepicker_dashboard', 
+                        useFadeInOut: !Browser.ie 
+    });
+
+    date_disable('open_date', 'open_ignore');
+    date_disable('close_date', 'close_ignore');
+});
