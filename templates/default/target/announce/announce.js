818443a6ff9403635b639089bed9884b017ca537
Chris <chris@starforge.co.uk>
2011-11-16 17:18:08 +0000

    Updated message form so that dates go into hidden fields as unix timestamps.

--- templates/default/target/announce/announce.js
+++ templates/default/target/announce/announce.js
@@ -1,27 +1,36 @@
 function date_disable(datefield, control) {
     var checked = $(control).get("checked");
 
-    $(datefield).set("disabled", checked);
+    $(datefield + "_pick").set("disabled", checked);
 
     // clear the contents if the field is disabled
     if(checked) {
+        $(datefield + "_pick").set('value', '');
         $(datefield).set('value', '');
     }
 }
 
 window.addEvent('domready', function() {
     Locale.use('en-GB')
-    new Picker.Date($('open_date'), { 
+    new Picker.Date($('open_date_pick'), { 
                         timePicker: true, 
+                        yearPicker: true, 
                         positionOffset: {x: 5, y: 0}, 
                         pickerClass: 'datepicker_dashboard', 
-                        useFadeInOut: !Browser.ie 
+                        useFadeInOut: !Browser.ie,
+                        onSelect: function(date){ 
+                            $('open_date').set('value', date.format('%s')); 
+                        }  
     });
-    new Picker.Date($('close_date'), { 
+    new Picker.Date($('close_date_pick'), { 
                         timePicker: true, 
+                        yearPicker: true, 
                         positionOffset: {x: 5, y: 0}, 
                         pickerClass: 'datepicker_dashboard', 
-                        useFadeInOut: !Browser.ie 
+                        useFadeInOut: !Browser.ie,
+                        onSelect: function(date){ 
+                            $('close_date').set('value', date.format('%s')); 
+                        }   
     });
 
     date_disable('open_date', 'open_ignore');
