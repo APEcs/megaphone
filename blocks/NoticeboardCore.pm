c09d882ec09ad27182cd909fe22e1cbfbee0b7e8
Chris <chris@starforge.co.uk>
2011-12-13 14:36:08 +0000

    Added very basic Noticeboard core class.

---
+++ blocks/NoticeboardCore.pm
@@ -0,0 +1,44 @@
+## @file
+# This file contains the implementation of the core noticeboard features.
+#
+# @author  Chris Page &lt;chris@starforge.co.uk&gt;
+# @version 1.0
+# @date    13 December 2011
+# @copy    2011, Chris Page &lt;chris@starforge.co.uk&gt;
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package NoticeboardCore;
+
+## @class NoticeboardCore
+# Implementation of the basic message browser interface. This presents
+# the user with a calendar and message list from which they may view
+# messages.
+use strict;
+use base qw(Block); # This class extends Block
+use Logging qw(die_log);
+use Utils qw(is_defined_numeric);
+
+# ============================================================================
+#  Interface functions
+
+## @method $ page_display()
+# Generate the page content for this module.
+sub page_display {
+    my $self = shift;
+
+    # very simple template load, doesn't need anything fancy here...
+    return $self -> {"template"} -> load_template("noticeboard.tem", {"***extrahead***" => ""});
+}
+
+1;
